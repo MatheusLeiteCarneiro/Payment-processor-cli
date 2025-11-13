0443cff88345b470a561c6034aaf906170d399a5
MatheusLeiteCarneiro <matheus.leite.carneiro@gmail.com>
2025-11-13 18:04:35 -0300

    feat: create contract class base

--- src/model/entities/Contract.java
+++ src/model/entities/Contract.java
@@ -1,4 +1,49 @@
 package model.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Contract {
+    private int number;
+    private String payerName;
+    private double basePrice;
+    private List<Installment> installmentList = new ArrayList<>();
+
+    public Contract() {
+    }
+
+    public Contract(int number, String payerName, double basePrice) {
+        this.number = number;
+        this.payerName = payerName;
+        this.basePrice = basePrice;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getPayerName() {
+        return payerName;
+    }
+
+    public void setPayerName(String payerName) {
+        this.payerName = payerName;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(double basePrice) {
+        this.basePrice = basePrice;
+    }
+
+    public List<Installment> getInstallmentList() {
+        return installmentList;
+    }
+
 }
