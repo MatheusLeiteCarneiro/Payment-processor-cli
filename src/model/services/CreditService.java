d48ffb001b103201f3e26f177caa754a61e4ecc6
MatheusLeiteCarneiro <matheus.leite.carneiro@gmail.com>
2025-11-13 18:48:32 -0300

    create: CreditService payment calculation method

--- src/model/services/CreditService.java
+++ src/model/services/CreditService.java
@@ -1,4 +1,18 @@
 package model.services;
 
-public class CreditService {
+import model.entities.Installment;
+
+public class CreditService implements PaymentService{
+    private static final double CREDIT_BASE_FEE = 0.02;
+    private static final double CREDIT_MONTHLY_INTEREST = 0.015;
+
+    @Override
+    public double paymentProcessing(double value, int installmentAmount) {
+        double total = value + value * CREDIT_BASE_FEE;
+        if(installmentAmount > 3){
+            total = value * Math.pow(1 + CREDIT_MONTHLY_INTEREST, installmentAmount);
+            total += value * CREDIT_BASE_FEE;
+        }
+        return total;
+    }
 }
