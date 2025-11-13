b29e44297b95411f917e6888a4a9228cc018550a
MatheusLeiteCarneiro <matheus.leite.carneiro@gmail.com>
2025-11-13 18:09:30 -0300

    feat: create installment class base

--- src/model/entities/Installment.java
+++ src/model/entities/Installment.java
@@ -1,4 +1,32 @@
 package model.entities;
 
+import java.time.LocalDate;
+
 public class Installment {
+    private LocalDate dueDate;
+    private double price;
+
+    public Installment() {
+    }
+
+    public Installment(LocalDate dueDate, double price) {
+        this.dueDate = dueDate;
+        this.price = price;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
