1d6e525653978ea07d2ecff25cc7df41d8144eb9
MatheusLeiteCarneiro <matheus.leite.carneiro@gmail.com>
2025-11-13 18:23:16 -0300

    feat: create ContactService class fields and methods

--- src/model/services/ContractService.java
+++ src/model/services/ContractService.java
@@ -1,4 +1,29 @@
 package model.services;
 
+import model.entities.Contract;
+import model.entities.Installment;
+
+import java.time.LocalDate;
+
 public class ContractService {
+    PaymentService paymentService;
+
+    public ContractService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    public PaymentService getPaymentService() {
+        return paymentService;
+    }
+
+    public void processInstallments(Contract contract, int months){
+        LocalDate contractDate = contract.getDate();
+        double contractValue = contract.getBasePrice();
+        for (int i = 1; i <= months; i++){
+            LocalDate dueDate = contractDate.plusMonths(i);
+            double installmentPrice = paymentService.paymentProcessing(contractValue, i);
+            contract.addInstallment(new Installment(dueDate, installmentPrice));
+        }
+
+    }
 }
